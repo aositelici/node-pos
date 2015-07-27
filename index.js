d5af68c78947004372b1ed4a7852d02f6b58468e
liya <liya013@live.com>
2015-07-27 11:03:00 +0800

    add index.js and seprate main.js

---
+++ index.js
@@ -0,0 +1,12 @@
+ var print = require('./main/main');
+ print.printReceipt([
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2',
+      'ITEM000005',
+      'ITEM000005',
+      'ITEM000005'
+    ]) ;
--- main/main.js
+++ main/main.js
@@ -1,32 +1,23 @@
-//function printReceipt(tags) {
+exports.printReceipt = function(tags) {
 var Scanner = require('./model/scanner');
 var Cart = require('./model/cart');
 var Pos = require('./model/pos');
 var PromotionCalculator = require('./model/promotion-calculator');
-var fixtures = require('/home/me/Documents/nodejs/pos_v2/test/fixtures');
+var fixtures = require('../test/fixtures');
 var CartItem = require('./model/cart-item');
-  tags=[
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2',
-      'ITEM000005',
-      'ITEM000005',
-      'ITEM000005'
-    ];
-  var scanner = new Scanner();
-  var cart = new Cart();
 
-  var pos = new Pos(cart,scanner);
-  var cartItems = pos.scan(tags);
+var scanner = new Scanner();
+var cart = new Cart();
 
-  var promotions = fixtures.loadPromotions();
-  var amount = cart.calculateAmount();
-  var salesCounts = PromotionCalculator.getSalesCounts(promotions,cartItems);
-  var salesAmounts = PromotionCalculator.calculateSalesAmount(promotions,cartItems);
-  var receipt = pos.printReceipt (cartItems,amount,salesAmounts,salesCounts);
+var pos = new Pos(cart,scanner);
+var cartItems = pos.scan(tags);
 
-  console.log(receipt);
-//}
+var promotions = fixtures.loadPromotions();
+var amount = cart.calculateAmount();
+var salesCounts = PromotionCalculator.getSalesCounts(promotions,cartItems);
+var salesAmounts = PromotionCalculator.calculateSalesAmount(promotions,cartItems);
+var receipt = pos.printReceipt (cartItems,amount,salesAmounts,salesCounts);
+
+console.log(receipt);
+}
+
